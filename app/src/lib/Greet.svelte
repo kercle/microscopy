edb7cca8437c2604526d5f41d63452c07bb37a90
Clemens Kerschbaumer <clemens.kerschbaumer@gmail.com>
2024-06-30 01:39:54 +0200

    Add shadcn-svelte

---
+++ app/src/lib/Greet.svelte
@@ -0,0 +1,17 @@
+<script>
+	import { invoke } from '@tauri-apps/api/core';
+	import Button from '$lib/components/ui/button/button.svelte';
+
+	let name = '';
+	let greetMsg = '';
+
+	async function greet() {
+		greetMsg = await invoke('greet', { name });
+	}
+</script>
+
+<div>
+	<input id="greet-input" placeholder="Enter a name..." bind:value={name} />
+	<Button on:click={greet}>Button</Button>
+	<p>{greetMsg}</p>
+</div>
