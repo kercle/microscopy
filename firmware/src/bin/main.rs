015d25042f0cfecf1e75286acebaac73c70fd4a3
Clemens Kerschbaumer <clemens.kerschbaumer@gmail.com>
2025-09-20 23:26:03 +0200

    Toggle of GPIO2 and printing 'Hello World!' from Esp32.

--- firmware/src/bin/main.rs
+++ firmware/src/bin/main.rs
@@ -1,44 +1,33 @@
 #![no_std]
 #![no_main]
-#![deny(
-    clippy::mem_forget,
-    reason = "mem::forget is generally not safe to do with esp_hal types, especially those \
-    holding buffers for the duration of a data transfer."
-)]
 
+use esp_backtrace as _;
 use esp_hal::{
-    clock::CpuClock,
+    delay::Delay,
+    gpio::{Level, Output, OutputConfig},
     main,
-    time::{Duration, Instant},
 };
+use esp_println::println;
 
-
-#[panic_handler]
-fn panic(_: &core::panic::PanicInfo) -> ! {
-    loop {}
-}
-
-extern crate alloc;
-
-// This creates a default app-descriptor required by the esp-idf bootloader.
-// For more information see: <https://docs.espressif.com/projects/esp-idf/en/stable/esp32/api-reference/system/app_image_format.html#application-description>
 esp_bootloader_esp_idf::esp_app_desc!();
 
 #[main]
 fn main() -> ! {
-    // generator version: 0.5.0
+    let peripherals = esp_hal::init(esp_hal::Config::default());
 
+    println!("Hello world!");
 
-    let config = esp_hal::Config::default().with_cpu_clock(CpuClock::max());
-    let _peripherals = esp_hal::init(config);
+    // Set GPIO7 as an output, and set its state high initially.
+    let mut led = Output::new(peripherals.GPIO2, Level::Low, OutputConfig::default());
 
-    esp_alloc::heap_allocator!(size: 64 * 1024);
+    led.set_high();
 
+    // Initialize the Delay peripheral, and use it to toggle the LED state in a
+    // loop.
+    let delay = Delay::new();
 
     loop {
-        let delay_start = Instant::now();
-        while delay_start.elapsed() < Duration::from_millis(500) {}
+        led.toggle();
+        delay.delay_millis(2000);
     }
-
-    // for inspiration have a look at the examples at https://github.com/esp-rs/esp-hal/tree/esp-hal-v1.0.0-rc.0/examples/src/bin
 }
