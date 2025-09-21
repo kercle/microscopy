b54e199f70fdcdad477ddd4f3542c7f187481495
Clemens Kerschbaumer <clemens.kerschbaumer@gmail.com>
2025-09-21 15:40:32 +0200

    WIP: Uart communication.

--- software/firmware/src/main.rs
+++ software/firmware/src/main.rs
@@ -10,6 +10,7 @@ use esp_hal::timer::timg::TimerGroup;
 use esp_hal::{
     clock::CpuClock,
     gpio::{Level, Output, OutputConfig},
+    uart::{Config as UartConfig, Uart, UartRx, UartTx},
 };
 
 use embassy_executor::Spawner;
@@ -24,6 +25,33 @@ extern crate alloc;
 
 esp_bootloader_esp_idf::esp_app_desc!();
 
+#[embassy_executor::task]
+async fn uart_rx_task(mut rx: UartRx<'static>, step: Output<'static>) {
+    let mut buf = [0u8; 64];
+
+    loop {
+        let n = rx.read_async(&mut buf).await.unwrap();
+        for &b in &buf[..n] {
+            if *b == b's' {
+                for _ in 0..50 {
+                    step.set_high();
+                    Timer::after(Duration::from_millis(1)).await;
+                    step.set_low();
+                    Timer::after(Duration::from_millis(1)).await;
+                }
+            }
+        }
+    }
+}
+
+#[embassy_executor::task]
+async fn uart_tx_task(mut tx: UartTx<'static>) {
+    loop {
+        tx.write_async(b"ok\n").await.ok();
+        embassy_time::Timer::after_millis(1000).await;
+    }
+}
+
 #[esp_hal_embassy::main]
 async fn main(spawner: Spawner) {
     let config = esp_hal::Config::default().with_cpu_clock(CpuClock::max());
@@ -33,6 +61,15 @@ async fn main(spawner: Spawner) {
     let _dir = Output::new(peripherals.GPIO25, Level::Low, OutputConfig::default());
     let _ms1 = Output::new(peripherals.GPIO32, Level::Low, OutputConfig::default());
     let _ms2 = Output::new(peripherals.GPIO33, Level::Low, OutputConfig::default());
+
+     let uart1 = Uart::new(
+        peripherals.UART1,
+        peripherals.GPIO17, // TX pin
+        peripherals.GPIO16, // RX pin
+        UartConfig::default().baudrate(115_200),
+    );
+
+    let (tx, rx) = uart1.split();
 
     esp_alloc::heap_allocator!(size: 64 * 1024);
 
@@ -46,7 +83,8 @@ async fn main(spawner: Spawner) {
         Timer::after(Duration::from_millis(1)).await;
     }
 
-    let _ = spawner;
+    spawner.must_spawn(uart_tx_task(tx));
+    spawner.must_spawn(uart_rx_task(rx, step));
 
     loop {
         Timer::after(Duration::from_secs(1)).await;
