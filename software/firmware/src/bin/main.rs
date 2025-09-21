8b2301091e259f4638fbc71532eeadb00010e499
Clemens Kerschbaumer <clemens.kerschbaumer@gmail.com>
2025-09-21 14:45:41 +0200

    Use embassy for firmware.

--- software/firmware/src/bin/main.rs
+++ software/firmware/src/bin/main.rs
@@ -1,49 +1,54 @@
 #![no_std]
 #![no_main]
+#![deny(
+    clippy::mem_forget,
+    reason = "mem::forget is generally not safe to do with esp_hal types, especially those \
+    holding buffers for the duration of a data transfer."
+)]
 
-// MS1 GPIO32
-// MS2 GPIO33
-// DIR GPIO25
-// STEP GPIO26
-
-use esp_backtrace as _;
+use esp_hal::timer::timg::TimerGroup;
 use esp_hal::{
-    delay::Delay,
+    clock::CpuClock,
     gpio::{Level, Output, OutputConfig},
-    main,
 };
+
+use embassy_executor::Spawner;
+use embassy_time::{Duration, Timer};
+
+#[panic_handler]
+fn panic(_: &core::panic::PanicInfo) -> ! {
+    loop {}
+}
+
+extern crate alloc;
 
 esp_bootloader_esp_idf::esp_app_desc!();
 
-#[main]
-fn main() -> ! {
-    let peripherals = esp_hal::init(esp_hal::Config::default());
-
-    // Set GPIO7 as an output, and set its state high initially.
-    let mut led = Output::new(peripherals.GPIO2, Level::Low, OutputConfig::default());
+#[esp_hal_embassy::main]
+async fn main(spawner: Spawner) {
+    let config = esp_hal::Config::default().with_cpu_clock(CpuClock::max());
+    let peripherals = esp_hal::init(config);
 
     let mut step = Output::new(peripherals.GPIO26, Level::Low, OutputConfig::default());
-    let mut dir = Output::new(peripherals.GPIO25, Level::High, OutputConfig::default());
+    let mut dir = Output::new(peripherals.GPIO25, Level::Low, OutputConfig::default());
     let _ms1 = Output::new(peripherals.GPIO32, Level::Low, OutputConfig::default());
     let _ms2 = Output::new(peripherals.GPIO33, Level::Low, OutputConfig::default());
 
-    dir.set_high();
+    esp_alloc::heap_allocator!(size: 64 * 1024);
+
+    let timer0 = TimerGroup::new(peripherals.TIMG1);
+    esp_hal_embassy::init(timer0.timer0);
 
     for _ in 0..100 {
         step.set_high();
-        Delay::new().delay_millis(10);
+        Timer::after(Duration::from_millis(1)).await;
         step.set_low();
-        Delay::new().delay_millis(10);
+        Timer::after(Duration::from_millis(1)).await;
     }
 
-    led.set_high();
-
-    // Initialize the Delay peripheral, and use it to toggle the LED state in a
-    // loop.
-    let delay = Delay::new();
+    let _ = spawner;
 
     loop {
-        led.toggle();
-        delay.delay_millis(2000);
+        Timer::after(Duration::from_secs(1)).await;
     }
 }
