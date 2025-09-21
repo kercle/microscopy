af6299746fa55481683ed73973053a713eca7502
Clemens Kerschbaumer <clemens.kerschbaumer@gmail.com>
2025-09-21 14:17:14 +0200

    Remove std bindings from com lib.

--- software/communication/src/lib.rs
+++ software/communication/src/lib.rs
@@ -1,3 +1,13 @@
-pub fn test() {
-    println!("Test");
+#![no_std]
+
+pub enum Axes {
+    X,
+    Y,
+    Z,
 }
+
+pub enum Commands {
+    MoveSteps { axis: Axes, steps: i32 },
+}
+
+pub fn test() {}
