e305ee7a604902f026d5a9438aa430ad5f8b2967
Clemens Kerschbaumer <clemens.kerschbaumer@gmail.com>
2025-09-21 02:02:13 +0200

    Testing stage z-axis motor.

--- firmware/src/bin/main.rs
+++ firmware/src/bin/main.rs
@@ -1,6 +1,11 @@
 #![no_std]
 #![no_main]
 
+// MS1 GPIO32
+// MS2 GPIO33
+// DIR GPIO25
+// STEP GPIO26
+
 use esp_backtrace as _;
 use esp_hal::{
     delay::Delay,
@@ -19,6 +24,20 @@ fn main() -> ! {
 
     // Set GPIO7 as an output, and set its state high initially.
     let mut led = Output::new(peripherals.GPIO2, Level::Low, OutputConfig::default());
+
+    let mut step = Output::new(peripherals.GPIO26, Level::Low, OutputConfig::default());
+    let mut dir = Output::new(peripherals.GPIO25, Level::High, OutputConfig::default());
+    let mut ms1 = Output::new(peripherals.GPIO32, Level::Low, OutputConfig::default());
+    let mut ms2 = Output::new(peripherals.GPIO33, Level::Low, OutputConfig::default());
+
+    dir.set_high();
+
+    for _ in 0..100 {
+        step.set_high();
+        Delay::new().delay_millis(10);
+        step.set_low();
+        Delay::new().delay_millis(10);
+    }
 
     led.set_high();
 
