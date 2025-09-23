008fa0572cf40724296534bab087b7db7cb1e170
Clemens Kerschbaumer <clemens.kerschbaumer@gmail.com>
2025-09-23 16:14:03 +0200

    Refactor command parsing for easier extentiability.

--- software/communication/src/parse.rs
+++ software/communication/src/parse.rs
@@ -6,6 +6,37 @@ use regex::Regex;
 
 use crate::{HostCommand, StageMotorCmd};
 
+impl StageMotorCmd {
+    fn parse_move_steps(s: &str) -> Option<Self> {
+        let r = Regex::new(r"^steps:(-?\d+),(\d+)$").unwrap();
+
+        if let Some(caps) = r.captures(s) {
+            let steps = if let Some(step_str) = caps.get(1) {
+                step_str.as_str().parse::<i32>().ok()
+            } else {
+                return None;
+            };
+
+            let step_delay_us = if let Some(delay_str) = caps.get(2) {
+                delay_str.as_str().parse::<u32>().ok()
+            } else {
+                return None;
+            };
+
+            if let (Some(steps), Some(step_delay_us)) = (steps, step_delay_us) {
+                Some(StageMotorCmd::MoveSteps {
+                    steps,
+                    step_delay_us,
+                })
+            } else {
+                None
+            }
+        } else {
+            None
+        }
+    }
+}
+
 impl fmt::Display for StageMotorCmd {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         match self {
@@ -23,27 +54,10 @@ impl FromStr for StageMotorCmd {
     type Err = String;
 
     fn from_str(s: &str) -> Result<Self, Self::Err> {
-        let r = Regex::new(r"^steps:(-?\d+),(\d+)$").map_err(|e| e.to_string())?;
-
-        if let Some(caps) = r.captures(s) {
-            let steps = caps
-                .get(1)
-                .ok_or("Missing steps")?
-                .as_str()
-                .parse::<i32>()
-                .map_err(|e| e.to_string())?;
-            let step_delay_us = caps
-                .get(2)
-                .ok_or("Missing step_delay_us")?
-                .as_str()
-                .parse::<u32>()
-                .map_err(|e| e.to_string())?;
-            Ok(StageMotorCmd::MoveSteps {
-                steps,
-                step_delay_us,
-            })
+        if let Some(cmd) = StageMotorCmd::parse_move_steps(s) {
+            Ok(cmd)
         } else {
-            Err("Invalid command format".to_string())
+            Err("Invalid StageMotorCmd format".to_string())
         }
     }
 }
