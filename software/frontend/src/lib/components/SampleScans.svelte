94adc8e8ada55da989e05091209f8ff1b21edb9c
Clemens Kerschbaumer <clemens.kerschbaumer@gmail.com>
2025-10-31 03:31:40 +0100

    Fine-tune GUI.

--- software/frontend/src/lib/components/SampleScans.svelte
+++ software/frontend/src/lib/components/SampleScans.svelte
@@ -12,48 +12,77 @@
 		uuid: string;
 	};
 
-	let frameSlider: HTMLInputElement;
-    let previewImage: HTMLImageElement;
+	let startOffsetInput: HTMLInputElement;
+	let stopOffsetInput: HTMLInputElement;
+	let stepsBetweenInput: HTMLInputElement;
 
-	let data: ScanEntry[] = [];
-	let selectedIdx: number | null = null;
+	let frameSlider: HTMLInputElement | undefined = $state();
+	let previewImage: HTMLImageElement | undefined = $state();
+
+	let data: ScanEntry[] = $state([]);
+	let selectedIdx: number | null = $state(null);
 
 	const selectIdx = (idx: number) => {
 		selectedIdx = idx;
-		frameSlider.value = '0';
+
+		if (frameSlider) {
+			frameSlider.value = '0';
+		}
 	};
 
-    const updatePreviewImage = () => {
-        if (selectedIdx === null) return;
-        previewImage.src = `/api/z_scan_thumbnail/${data[selectedIdx].uuid}/${frameSlider.value}/800`;
-    };
+	const updatePreviewImage = () => {
+		if (selectedIdx === null || frameSlider === undefined || previewImage === undefined) return;
+		previewImage.src = `/api/z_scan_thumbnail/${data[selectedIdx].uuid}/${frameSlider.value}/800`;
+	};
+
+	const updateZScanData = async () => {
+		const res = await fetch('/api/list_z_scans');
+		if (!res.ok) throw new Error(`HTTP ${res.status}`);
+		data = await res.json();
+
+		// data = [
+		// 	{
+		// 		timestamp: '2024-06-01T12:00:00Z',
+		// 		frame_count: 150,
+		// 		relative_start_pos: 0,
+		// 		relative_stop_pos: 3000,
+		// 		steps_between_layers: 20,
+		// 		uuid: '123e4567-e89b-12d3-a456-426614174000'
+		// 	},
+		// 	{
+		// 		timestamp: '2024-06-02T15:30:00Z',
+		// 		frame_count: 200,
+		// 		relative_start_pos: 100,
+		// 		relative_stop_pos: 4100,
+		// 		steps_between_layers: 20,
+		// 		uuid: '123e4567-e89b-12d3-a456-426614174001'
+		// 	}
+		// ];
+
+		selectedIdx = data.length > 0 ? 0 : null;
+	};
+
+	const startZScan = () => {
+		const startOffset = parseInt(startOffsetInput.value, 10);
+		const stopOffset = parseInt(stopOffsetInput.value, 10);
+		const stepsBetween = parseInt(stepsBetweenInput.value, 10);
+
+		fetch(`/api/z_scan/${startOffset}/${stopOffset}/${stepsBetween}`)
+			.then((res) => {
+				if (!res.ok) throw new Error(`HTTP ${res.status}`);
+				return res.json();
+			})
+			.then((json) => {
+				data.push(json);
+			})
+			.catch((err) => {
+				console.error('z-scan failed: ', err);
+			});
+	};
 
 	onMount(async () => {
 		try {
-			const res = await fetch('/api/list_z_scans');
-			if (!res.ok) throw new Error(`HTTP ${res.status}`);
-			data = await res.json();
-
-			// data = [
-			// 	{
-			// 		timestamp: '2024-06-01T12:00:00Z',
-			// 		frame_count: 150,
-			// 		relative_start_pos: 0,
-			// 		relative_stop_pos: 3000,
-			// 		steps_between_layers: 20,
-			// 		uuid: '123e4567-e89b-12d3-a456-426614174000'
-			// 	},
-			// 	{
-			// 		timestamp: '2024-06-02T15:30:00Z',
-			// 		frame_count: 200,
-			// 		relative_start_pos: 100,
-			// 		relative_stop_pos: 4100,
-			// 		steps_between_layers: 20,
-			// 		uuid: '123e4567-e89b-12d3-a456-426614174001'
-			// 	}
-			// ];
-
-			selectedIdx = data.length > 0 ? 0 : null;
+			await updateZScanData();
 		} catch (err) {
 			console.error('Failed to fetch data:', err);
 		}
@@ -63,18 +92,50 @@
 <div
 	class="flex flex-col-reverse gap-2 overflow-y-auto lg:h-[calc(100vh-90px)] lg:flex-row lg:overflow-y-hidden"
 >
-	<div
-		class="divide-base-300 flex flex-col divide-y rounded-md lg:h-[calc(100vh-90px)] lg:overflow-y-auto"
-	>
+	<div class="mb-8 flex min-w-60 flex-col rounded-md lg:h-[calc(100vh-90px)] lg:overflow-y-auto">
+		<div class="mb-4 flex flex-col gap-2 text-sm">
+			<div class="mt-2 flex flex-row items-center">
+				<div class="mr-auto">Start offset:</div>
+				<input
+					bind:this={startOffsetInput}
+					type="number"
+					class="input input-ghost w-17 h-6 min-h-0 py-0 text-right"
+					value="200"
+				/>
+				<span class="badge badge-sm text-accent text-xs">steps</span>
+			</div>
+			<div class="flex flex-row">
+				<div class="mr-auto">Stop offset:</div>
+				<input
+					bind:this={stopOffsetInput}
+					type="number"
+					class="input input-ghost w-17 h-6 min-h-0 py-0 text-right"
+					value="-200"
+				/>
+				<span class="badge badge-sm text-accent text-xs">steps</span>
+			</div>
+			<div class="flex flex-row">
+				<div class="mr-auto">Steps between:</div>
+				<input
+					bind:this={stepsBetweenInput}
+					type="number"
+					class="input input-ghost w-17 h-6 min-h-0 py-0 text-right"
+					value="10"
+					min="1"
+				/>
+				<span class="badge badge-sm text-accent text-xs">steps</span>
+			</div>
+			<button class="btn btn-primary mx-2 mt-2" onclick={startZScan}>Start Z-Scan</button>
+		</div>
 		{#each data as entry, idx}
 			<button
 				class="hover:bg-base-200 flex cursor-pointer flex-row items-start gap-8 p-2 text-left"
 				onclick={() => selectIdx(idx)}
 			>
 				<div class="pointer-events-none">
-					<img src="/api/z_scan_thumbnail/{entry.uuid}/0/100" alt="Thumbnail" class="rounded-md" />
+					<img src="/api/z_scan_thumbnail/{entry.uuid}/0/80" alt="Thumbnail" class="rounded-md" />
 				</div>
-				<div class="flex-2 w-100 pointer-events">
+				<div class="flex-2 w-90 pointer-events">
 					<div class="text-sm font-bold">
 						{new Date(entry.timestamp).toLocaleString()}
 					</div>
@@ -90,7 +151,7 @@
 		{#if selectedIdx !== null}
 			<div class="relative w-full">
 				<img
-                    bind:this={previewImage}
+					bind:this={previewImage}
 					src="/api/z_scan_thumbnail/{data[selectedIdx].uuid}/0/800"
 					alt="Preview"
 					class="w-full rounded-md"
@@ -131,9 +192,26 @@
 					max={data[selectedIdx].frame_count - 1}
 					value="0"
 					class="range absolute bottom-0 left-0 mb-4 ml-[1rem] w-[calc(100%-2rem)] opacity-70 hover:opacity-100"
-                    onchange={updatePreviewImage}
+					onchange={updatePreviewImage}
 				/>
 			</div>
 		{/if}
 	</div>
 </div>
+
+<style>
+	/* Chrome, Safari, Edge */
+	input[type='number']::-webkit-inner-spin-button,
+	input[type='number']::-webkit-outer-spin-button {
+		-webkit-appearance: none;
+		appearance: none;
+		margin: 0;
+	}
+
+	/* Firefox */
+	input[type='number'] {
+		-moz-appearance: textfield;
+		appearance: textfield;
+		margin: 0;
+	}
+</style>
